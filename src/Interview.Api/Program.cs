120851c75931d40d1cdc8452f5c5f7629024910a
Eduardo Antunes <eduardosdantunes@live.com>
2022-05-13 06:15:59 -0300

    refactoring code - adding status code return and WithTags

--- src/Interview.Api/Program.cs
+++ src/Interview.Api/Program.cs
@@ -22,44 +22,78 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 await app.Services.EnsureCreated();
 
-app.MapPost("/api/v1/companies", async (ICompanyRepository repository, CompanyModel model, CancellationToken cancellation) =>
-{
-    var isin = new CompanyIsin(model.Isin);
-    var company = new Company(model.Name, model.Exchange, model.Ticker, isin, model.WebSite);
-    var result = await repository.CreateCompanyAsync(company, cancellation);
+app.MapPost("/api/v1/companies", async (
+    ICompanyRepository repository, 
+    CompanyModel model, 
+    CancellationToken cancellation) =>
+    {
+        var isin = new CompanyIsin(model.Isin);
+        var company = new Company(model.Name, model.Exchange, model.Ticker, isin, model.WebSite);
+        var result = await repository.CreateCompanyAsync(company, cancellation);
 
-    return Results.CreatedAtRoute("GetCompanyById", new { result.Id });
-}).WithName("CreateCompany");
+        return result is not null
+            ? Results.CreatedAtRoute("GetCompanyById", new { ID = result.Id })
+            : Results.BadRequest("Error to save company!");
+    })
+    .Produces<Company>(StatusCodes.Status201Created)
+    .Produces<Company>(StatusCodes.Status400BadRequest)
+    .WithName("CreateCompany")
+    .WithTags("Companies");
 
-app.MapGet("/api/v1/companies/{id:int}", async (ICompanyRepository repository, int id, CancellationToken cancellation) =>
-{
-    var result = await repository.FindByIdAsync(id, cancellation);
+app.MapGet("/api/v1/companies/{id:int}", async (
+    ICompanyRepository repository, 
+    int id, 
+    CancellationToken cancellation) =>
+    {
+        var result = await repository.FindByIdAsync(id, cancellation);
 
-    return result == null ? Results.NotFound() : Results.Ok(result);
-}).WithName("GetCompanyById");
+        return result == null ? Results.NotFound() : Results.Ok(result);
+    })
+    .Produces(StatusCodes.Status200OK)
+    .Produces(StatusCodes.Status404NotFound)
+    .WithName("GetCompanyById")
+    .WithTags("Companies");
 
-app.MapGet("/api/v1/companies/isin/{isin}", async (ICompanyRepository repository, string isin, CancellationToken cancellation) =>
-{
-    var result = await repository.FindByIsinAsync(isin, cancellation);
+app.MapGet("/api/v1/companies/isin/{isin}", async (
+    ICompanyRepository repository, 
+    string isin, 
+    CancellationToken cancellation) =>
+    {
+        var result = await repository.FindByIsinAsync(isin, cancellation);
 
-    return result == null ? Results.NotFound() : Results.Ok(result);
-}).WithName("GetCompanyByIsin");
+        return result == null ? Results.NotFound() : Results.Ok(result);
+    })
+    .Produces(StatusCodes.Status200OK)
+    .Produces(StatusCodes.Status404NotFound)
+    .WithName("GetCompanyByIsin")
+    .WithTags("Companies");
 
-app.MapGet("/api/v1/companies", (ICompanyRepository repository, CancellationToken cancellation) =>
-{
-    var result = repository.GetAllCompanies(cancellation);
-    return Results.Ok(result);
+app.MapGet("/api/v1/companies", (
+    ICompanyRepository repository, 
+    CancellationToken cancellation) =>
+    {
+        var result = repository.GetAllCompanies(cancellation);
+        return Results.Ok(result);
+    })
+    .Produces(StatusCodes.Status200OK)
+    .WithName("GetCompanies")
+    .WithTags("Companies");
 
-}).WithName("GetCompanies");
+app.MapPut("/api/v1/companies/{id}", async (
+    ICompanyRepository repository, 
+    int id, CompanyModel model, 
+    CancellationToken cancellation) =>
+    {
+        var isin = new CompanyIsin(model.Isin);
+        var company = new Company(model.Name, model.Exchange, model.Ticker, isin, model.WebSite);
+        var result = await repository.SaveChangesAsync(id, company, cancellation);
 
-app.MapPut("/api/v1/companies/{id}", async (ICompanyRepository repository, int id, CompanyModel model, CancellationToken cancellation) =>
-{
-    var isin = new CompanyIsin(model.Isin);
-    var company = new Company(model.Name, model.Exchange, model.Ticker, isin, model.WebSite);
-    var result = await repository.SaveChangesAsync(id, company, cancellation);
+        return result == null ? Results.BadRequest("Problem to save this record!") : Results.Ok(company);
 
-    return result == null ? Results.NotFound() : Results.Ok(company);
-
-}).WithName("ChangeCompany");
+    })
+    .Produces(StatusCodes.Status204NoContent)
+    .Produces(StatusCodes.Status400BadRequest)
+    .WithName("ChangeCompany")
+    .WithTags("Companies");
 
 app.Run();

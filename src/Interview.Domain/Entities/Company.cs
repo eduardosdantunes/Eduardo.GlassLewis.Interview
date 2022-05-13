7b210851498b27c7306c125666b98710f322ebad
Eduardo Antunes <eduardosdantunes@live.com>
2022-05-13 01:52:18 -0300

    Adding Entitie Company and Isin

---
+++ src/Interview.Domain/Entities/Company.cs
@@ -0,0 +1,21 @@
+﻿namespace Interview.Domain.Entities;
+
+public class Company
+{
+    private Company() { }
+
+    public Company(string name, string exchange, string ticker, CompanyIsin isin)
+    {
+        Name = name;
+        Exchange = exchange;
+        Ticker = ticker;
+        Isin = isin;
+    }
+
+    public int Id { get; private set; }
+    public string Name { get; init; } = null!;
+    public string Exchange { get; init; } = null!;
+    public string Ticker { get; init; } = null!;
+    public CompanyIsin Isin { get; private set; } = null!;
+    public string? WebSite { get; set; }
+}
---
+++ src/Interview.Domain/Entities/CompanyIsin.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Interview.Domain.Entities;
+
+public record CompanyIsin
+{
+    public CompanyIsin(string value)
+    {
+        SetIsin(value);
+    }
+
+    public string Value { get; private set; } = string.Empty;
+
+    public void SetIsin(string value)
+    {
+        var match = Regex.Match(value, "([A-Z]{2})([A-Z0-9]{9})([0-9]{1})");
+        if (!match.Success) throw new Exception("invalid isin");
+        Value = value;
+    }
+}

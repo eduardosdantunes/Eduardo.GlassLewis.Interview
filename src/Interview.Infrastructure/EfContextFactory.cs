6f990458453c70d1b136d216405ed8cb7d127826
Eduardo Antunes <eduardosdantunes@live.com>
2022-05-13 01:58:37 -0300

    Adding DbContext (EF Core components)

---
+++ src/Interview.Infrastructure/EfContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Interview.Infrastructure;
+
+internal class EfContextFactory : IDesignTimeDbContextFactory<InterviewContext>
+{
+    public InterviewContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<InterviewContext>();
+        optionsBuilder.UseSqlServer("Data Source=localhost;Database=InterviewDb;Integrated Security=True");
+
+
+        return new InterviewContext(optionsBuilder.Options);
+    }
+}
---
+++ src/Interview.Infrastructure/InterviewContext.cs
@@ -0,0 +1,19 @@
+﻿using Interview.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Interview.Infrastructure;
+
+internal class InterviewContext : DbContext
+{
+    public InterviewContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+    }
+
+    public DbSet<Company> Companies { get; set; } = null!;
+}

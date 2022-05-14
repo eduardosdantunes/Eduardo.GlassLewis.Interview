906c30a0b05d58c1c49a57a021bf38b43391c4a9
Eduardo Antunes <eduardosdantunes@live.com>
2022-05-13 21:11:36 -0300

    Improving company entity

--- src/Interview.Domain/Entities/Company.cs
+++ src/Interview.Domain/Entities/Company.cs
@@ -6,17 +6,44 @@ public class Company
 
     public Company(string name, string exchange, string ticker, CompanyIsin isin, string? webSite = null)
     {
-        Name = name;
-        Exchange = exchange;
-        Ticker = ticker;
-        Isin = isin;
-        WebSite = webSite;
+        SetName(name);
+        SetExchange(exchange);
+        SetTicker(ticker);
+        this.Isin = isin;
+        this.WebSite = webSite;
     }
 
     public int Id { get; private set; }
-    public string Name { get; set; }
+    public string Name { get; set; } 
     public string Exchange { get; set; }
     public string Ticker { get; set; }
     public CompanyIsin Isin { get; set; } = null!;
     public string? WebSite { get; set; }
+
+    public void SetName(string Name)
+    {
+        if (string.IsNullOrEmpty(Name) || Name.Trim() == "")
+        {
+            throw new ArgumentNullException("Name");
+        }
+        this.Name = Name;
+    }
+
+    public void SetExchange(string Exchange)
+    {
+        if (string.IsNullOrEmpty(Exchange) || Exchange.Trim() == "")
+        {
+            throw new ArgumentNullException("Exchange");
+        }
+        this.Exchange = Exchange;
+    }
+
+    public void SetTicker(string Ticker)
+    {
+        if (string.IsNullOrEmpty(Ticker) || Ticker.Trim() == "")
+        {
+            throw new ArgumentNullException("Ticker");
+        }
+        this.Ticker = Ticker;
+    }
 }

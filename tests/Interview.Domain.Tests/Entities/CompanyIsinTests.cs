39b6dfe6c7f3220e74d653a09160e4a484df80e1
Eduardo Antunes <eduardosdantunes@live.com>
2022-05-13 21:12:39 -0300

    segregating tests

--- tests/Interview.Domain.Tests/Entities/CompanyIsinTests.cs
+++ tests/Interview.Domain.Tests/Entities/CompanyIsinTests.cs
@@ -1,31 +1,9 @@
-﻿using AutoFixture;
-using FluentAssertions;
+﻿using FluentAssertions;
 using Interview.Domain.Entities;
 using System.Threading.Tasks;
 using Xunit;
 
 namespace Interview.Domain.Tests.Entities;
-
-public class CompanyTests
-{
-    private readonly IFixture _fixture;
-
-    public CompanyTests()
-    {
-        _fixture = new Fixture();
-    }
-
-    [Fact]
-    public void Teste()
-    {
-        var name = _fixture.Create<string>();
-        var exchange = _fixture.Create<string>();
-        var ticker = _fixture.Create<string>();
-        var isin = new CompanyIsin("US45256BAD38");
-
-        var company = new Company(name, exchange, ticker, isin);
-    }
-}
 
 public class CompanyIsinTests
 {
@@ -76,7 +54,7 @@ public class CompanyIsinTests
     [Theory]
     [InlineData("US45256BAD38")]
     [InlineData("BR1234567891")]
-    public void SetIsinIsValid(string isinInput)
+    public void SetValidIsin(string isinInput)
     {
         // Arrange
         var isin = new CompanyIsin(isinInput);

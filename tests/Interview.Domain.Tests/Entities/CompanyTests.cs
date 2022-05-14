16afb0d948657f09d51981c650406b68eea7fa47
Eduardo Antunes <eduardosdantunes@live.com>
2022-05-13 21:12:08 -0300

    adding companyTests

---
+++ tests/Interview.Domain.Tests/Entities/CompanyTests.cs
@@ -0,0 +1,101 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using Interview.Domain.Entities;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Interview.Domain.Tests.Entities;
+
+public class CompanyTests
+{
+    private readonly IFixture _fixture;
+
+    public CompanyTests()
+    {
+        _fixture = new Fixture();
+    }
+
+    [Fact]
+    public void CompanyTypeTest()
+    {
+        var name = _fixture.Create<string>();
+        var exchange = _fixture.Create<string>();
+        var ticker = _fixture.Create<string>();
+        var isin = new CompanyIsin("US45256BAD38");
+        var webSite = _fixture.Create<string>();
+
+        var company = new Company(name, exchange, ticker, isin, webSite);
+
+        company.Should().Be(company);
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    public void SetInvalidName(string nameInput)
+    {
+        // Arrange
+        var isin = new CompanyIsin("US45256BAD38");
+
+        // Act
+        var exception = Record.Exception(() => new Company(nameInput, "exchange", "ticket", isin));
+
+        // Assert
+        exception.Should().NotBeNull();
+        exception.Message.Should().Contain("Value cannot be null");
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    public void SetInvalidExchange(string exchangeInput)
+    {
+        // Arrange
+        var isin = new CompanyIsin("US45256BAD38");
+
+        // Act
+        var exception = Record.Exception(() => new Company("name", exchangeInput, "ticket", isin));
+
+        // Assert
+        exception.Should().NotBeNull();
+        exception.Message.Should().Contain("Value cannot be null");
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    public void SetInvalidTicket(string ticketInput)
+    {
+        // Arrange
+        var isin = new CompanyIsin("US45256BAD38");
+
+        // Act
+        var exception = Record.Exception(() => new Company("name", "exchange", ticketInput, isin));
+
+        // Assert
+        exception.Should().NotBeNull();
+        exception.Message.Should().Contain("Value cannot be null");
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData("ANBA&$&@NSDS*!!)@*&$¨@,~;/;")]
+    [InlineData("https://github.com/eduardosdantunes/Eduardo.GlassLewis.Interview")]
+    [InlineData("http://www.apple.com")]
+    public void SetValidWebSite(string webSiteInput)
+    {
+        // Arrange
+        var isin = new CompanyIsin("US45256BAD38");
+
+        // Act
+        var exception = Record.Exception(() => new Company("name", "exchange", "ticket", isin, webSiteInput));
+
+        // Assert
+        exception.Should().BeNull();
+    }
+}

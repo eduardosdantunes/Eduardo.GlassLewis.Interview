5468d589594986cd693161c0c944e8779776a8db
Eduardo Antunes <eduardosdantunes@live.com>
2022-05-16 22:14:34 -0300

    adding Validating Get Companies

--- tests/Interview.Infrastructure.Tests/CompanyRepositoryTests.cs
+++ tests/Interview.Infrastructure.Tests/CompanyRepositoryTests.cs
@@ -70,6 +70,37 @@ namespace Interview.Infrastructure.Tests
         }
 
         [Fact]
+        public async Task ValidatingGetCompanies()
+        {
+            // Arrange
+            var builder = new DbContextOptionsBuilder<InterviewContext>().UseInMemoryDatabase(Guid.NewGuid().ToString());
+            var context = new InterviewContext(builder.Options);
+
+            var service = new CompanyRepository(context);
+
+            var name = _fixture.Create<string>();
+            var exchange = _fixture.Create<string>();
+            var ticker = _fixture.Create<string>();
+
+            var company1 = new Company(name, exchange, ticker, new CompanyIsin("US45256BAD38"));
+            var company2 = new Company(name, exchange, ticker, new CompanyIsin("US1104193065"));
+
+            // Act
+            await service.CreateCompanyAsync(company1, CancellationToken.None);
+            await service.CreateCompanyAsync(company2, CancellationToken.None);
+
+            // Act
+            var exception = await Record.ExceptionAsync(async () =>
+            {
+                service.GetAllCompanies(CancellationToken.None);
+            });
+
+            //Assert
+            exception.Should().BeNull();
+
+        }
+
+        [Fact]
         public async Task NotAllowToCreateMultipleCompaniesWithTheSameId()
         {
             // Arrange

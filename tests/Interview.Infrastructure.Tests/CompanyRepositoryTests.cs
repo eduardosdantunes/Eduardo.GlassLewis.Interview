0c3752606395dcd70227b4448aaf73bfd06cfd7f
Eduardo Antunes <eduardosdantunes@live.com>
2022-05-16 21:31:23 -0300

    Adding Test NotAllowSameId

--- tests/Interview.Infrastructure.Tests/CompanyRepositoryTests.cs
+++ tests/Interview.Infrastructure.Tests/CompanyRepositoryTests.cs
@@ -19,8 +19,9 @@ namespace Interview.Infrastructure.Tests
         }
 
         [Fact]
-        public async Task NotAllowToCreateMultipleCompaniesWithTheSameIsin()
+        public async Task NotAllowToCreateMultipleCompaniesWithTheSameId()
         {
+            // Arrange
             var builder = new DbContextOptionsBuilder<InterviewContext>().UseInMemoryDatabase(Guid.NewGuid().ToString());
             var context = new InterviewContext(builder.Options);
 
@@ -31,16 +32,18 @@ namespace Interview.Infrastructure.Tests
             var ticker = _fixture.Create<string>();
             var isin = new CompanyIsin("US45256BAD38");
 
-            var company1 = new Company(name, exchange, ticker, isin);
-            var company2 = new Company(name, exchange, ticker, isin);
+            var company = new Company(name, exchange, ticker, isin);
 
+            // Act
             var exception = await Record.ExceptionAsync(async () =>
             {
-                await service.CreateCompanyAsync(company1, CancellationToken.None);
-                await service.CreateCompanyAsync(company2, CancellationToken.None);
+                await service.CreateCompanyAsync(company, CancellationToken.None);
+                await service.CreateCompanyAsync(company, CancellationToken.None);
             });
 
+            //Assert
             exception.Should().NotBeNull();
+            exception.Message.Should().Contain("An item with the same key has already been added");
         }
     }
 }
